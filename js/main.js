7ffdd97ca63ff72a3cec2724a940804929a76d2f
Mariah Alderson <mariah.alderson@gmail.com>
2018-03-20 23:11:38 -0400

    added loop to populate images

--- js/main.js
+++ js/main.js
@@ -4,7 +4,31 @@
   let playScreen = document.querySelector('.startScreen'),
   	  playButton = document.querySelector('#start'),
   	  //cards = document.querySelector('.card');
-  	  cards = Array.from(document.querySelectorAll('.card'));
+
+      content = document.querySelector('#gameplayArea'),
+      imagePaths = ["images/cardFront.png",
+                    "images/cardFront2.png",
+                    "images/cardFront3.png",
+                    "images/cardFront4.png",
+                    "images/cardFront5.png"
+                    ],
+      list = document.createElement("ul");
+
+console.log(imagePaths[0]);
+
+ //run for loop to fill with <li> items
+  for(var i=0; i<imagePaths.length; i++) {
+    console.log(i);
+  //here we use innerHTML to write in images, using the values in the array
+    list.innerHTML += `<li><section class="container"><div class="card">
+             <img src="images/cardBack.png" class="front">
+             <img src="${imagePaths[i]}" alt="card" class="back">
+             </div></section></li>`
+  
+  //now that ul is filled with li and images append it to content div
+  content.appendChild(list);}
+
+let       cards = Array.from(document.querySelectorAll('.card'));
 
 
 //function to reveal game play after clicking "play" on start screen
@@ -29,28 +53,29 @@ function removeFlip(){
 }
 
 //shuffle cards
-function shuffle (array) {
-  var i = 0
-    , j = 0
-    , temp = null
-    , cards = array
+// function shuffle (array) {
+//   var i = 0
+//     , j = 0
+//     , temp = null
+//     , cards = array
 
-  for (i = array.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-}
+//   for (i = array.length - 1; i > 0; i -= 1) {
+//     j = Math.floor(Math.random() * (i + 1))
+//     temp = array[i]
+//     array[i] = array[j]
+//     array[j] = temp
+//   }
+// }
+
 
 
 //begin Game
-function openGame(){
-  beginGame();
-  shuffle(cards);
-}
+// function openGame(){
+//   beginGame();
+//   shuffle(cards);
+// }
 
-playButton.addEventListener('click', openGame);
+playButton.addEventListener('click', beginGame);
 //cards.addEventListener('click', flipCard);
 cards.forEach(card=>card.addEventListener('click', flipCard));
 console.log(cards);
