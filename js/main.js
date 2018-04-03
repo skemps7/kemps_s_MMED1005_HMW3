89d380347a9192b32de189af5ad1962e0a06a280
Mariah Alderson <mariah.alderson@gmail.com>
2018-04-03 12:34:36 -0400

    fisher-yates shuffle did it!

--- js/main.js
+++ js/main.js
@@ -3,14 +3,44 @@
 
   let playScreen = document.querySelector('.startScreen'),
   	  playButton = document.querySelector('#start'),
-  	  //cards = document.querySelector('.card');
-  	  cards = Array.from(document.querySelectorAll('.card'));
+      content = document.querySelector('#gameplayArea'),
+      imagePaths = ["images/cardFront.png",
+                    "images/cardFront2.png",
+                    "images/cardFront3.png",
+                    "images/cardFront4.png",
+                    "images/cardFront5.png"
+                    ],
+      list = document.createElement("ul");
+
 
 
 //function to reveal game play after clicking "play" on start screen
 function beginGame(){
 	playScreen.classList.add('showPiece');
 }
+
+
+//shuffle cards
+var i = imagePaths.length, j, temp;
+
+while(--i >= 0){
+    j = Math.floor(Math.random() * (i+1)); // Get random number ranging between 0 and i
+    temp = imagePaths[j];
+    imagePaths[j] = imagePaths[i];
+    imagePaths[i] = temp;
+
+console.log(imagePaths);
+
+    list.innerHTML += `<li><section class="container"><div class="card">
+             <img src="images/cardBack.png" class="front">
+             <img src="${imagePaths[i]}" alt="card" class="back">
+             </div></section></li>`
+  }
+  content.appendChild(list);
+
+
+
+let cards = Array.from(document.querySelectorAll('.card'));
 
 //timer
 var sec = 0; function pad ( val ) { return val > 9 ? val : "0" + val; } setInterval( function(){
@@ -28,29 +58,16 @@ function removeFlip(){
 	this.classList.remove('flipped');
 }
 
-//shuffle cards
-function shuffle (array) {
-  var i = 0
-    , j = 0
-    , temp = null
-    , cards = array
-
-  for (i = array.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-}
 
 
-//begin Game
-function openGame(){
-  beginGame();
-  shuffle(cards);
-}
 
-playButton.addEventListener('click', openGame);
+// //begin Game
+// function openGame(){
+//   beginGame();
+//   shuffle(imagePaths);
+// }
+
+playButton.addEventListener('click', beginGame);
 //cards.addEventListener('click', flipCard);
 cards.forEach(card=>card.addEventListener('click', flipCard));
 console.log(cards);
