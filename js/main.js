50e35ed93307e3ed931588547897a502c2714690
Trevor Van Rys <trevorvanrys@Trevors-MacBook-Pro-2.local>
2018-04-05 15:49:35 -0400

    minor updates

--- js/main.js
+++ js/main.js
@@ -15,11 +15,12 @@
                     "images/cardFront4.png",
                     "images/cardFront5.png"
                     ],
-      list = document.createElement("ul");
+      list = document.createElement("ul"),
+      flip =[];
 
 
 //function to reveal game play after clicking "play" on start screen
-function beginGame(){
+function beginGame() {
 	playScreen.classList.add('showPiece');
 }
 
@@ -27,7 +28,7 @@ function beginGame(){
 //shuffle cards
 var i = imagePaths.length, j, temp;
 
-while(--i >= 0){
+while(--i >= 0) {
     j = Math.floor(Math.random() * (i+1)); // Get random number ranging between 0 and i
     temp = imagePaths[j]; 
     imagePaths[j] = imagePaths[i];
@@ -37,53 +38,47 @@ while(--i >= 0){
              <img src="images/cardBack.png" class="front">
              <img src="${imagePaths[i]}" alt="card" class="back">
              </div></section></li>`
-  }
-  content.appendChild(list);
+}
 
+content.appendChild(list);
 
 let cards = Array.from(document.querySelectorAll('.card'));
-
 
 //timer
 var sec = 0; function pad ( val ) { return val > 9 ? val : "0" + val; } setInterval( function(){
    document.getElementById("seconds").innerHTML=pad(++sec%60);
    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10)); }, 1000);
 
-
-//flip card back over
-function removeFlip(){
+//flip card back over -> see the comment on line 72
+function removeFlip() {
   this.classList.remove('flipped');
 }
 
 //flip card
-function flipCard(){
+function flipCard() {
 	this.classList.add('flipped');
+
+  flip.push(this); // push the whole div into the array
+
+  checkCards(flip.length);
 }
 
-  let flip = Array.from(document.querySelectorAll('img .flipped'));
-  flip.push('.flipped');
-  var len = flip.length;
+  function match() {
 
-  function match(){
-      if(flip[0].src != flip[1].src){
-      removeFlip(flip[0]);
-      removeFlip(flip[1]);
-      enable();
+    if (flip[0].querySelector('.back').src != flip[1].querySelector('.back').src) {
+      removeFlip.call(flip[0]); // or you can do it like this: removeFlip(flip[0]);
+      removeFlip.call(flip[1]); // same as above -> but you'd have add card to the removeFlip function inside the round brackets
+
       flip = [];
     }
   }
 
-function check(len){
+function checkCards(len) {
   if(len===2){
     match();
   }
 }
 
-check(len);
-
 playButton.addEventListener('click', beginGame);
-//cards.addEventListener('click', flipCard);
-cards.forEach(card=>card.addEventListener('click', flipCard));
 
-
-
+cards.forEach(card => card.addEventListener('click', flipCard));
